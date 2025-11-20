bf29f4feb5d2cac1fdf30f2df5765455c0b511f2
CapRodri24 <201905170@est.umss.edu>
2025-11-19 22:55:27 -0400

    fix: importaciones

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_APP_TITLE: string
+  readonly VITE_MODE: string
+  // Agrega aquí todas tus variables de entorno...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
